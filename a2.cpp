52de5f9c6ed75d7e73e5c1c2a0db5287f36b62ef
dohoainam <64294945+leyamir@users.noreply.github.com>
2021-12-11 02:01:24 +0700

    Update a2.cpp

--- a2.cpp
+++ a2.cpp
@@ -2,51 +2,50 @@
 
 using namespace std;
 
+int INF = 1e9;
+
 
 void findkthshortest(int n, int m, int k, int edges[][3]){
 
     vector<vector<pair<int, int>> > map_(n + 1);
     for (int i = 0; i < m; i++) {
 
-        map_[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+        int from = edges[i][0];
+        int to = edges[i][1];
+        int d = edges[i][2];
+        map_[from].push_back({to, d});
     }
 
 
-    vector<vector<int> > dist(n + 1, vector<int>(k, 1e9));
-
-    priority_queue<pair<int, int>,
-                   vector<pair<int, int> >,
-                   greater<pair<int, int> > >
-
-        pq;
-
-    pq.push({0, 1});
+    vector<vector<int>> dist(n + 1, vector<int>(k, INF));
     dist[1][0] = 0;
+
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
+    pq.push({0,1});
 
 
     while (!pq.empty()) {
 
-        int u = pq.top().second;
-
-        int d = (pq.top().first);
+        int N = pq.top().second;
+        int D = (pq.top().first);
         pq.pop();
-        if (dist[u][k - 1] < d)
-            continue;
-        vector<pair<int, int> > v = map_[u];
 
-    for (int i = 0; i < v.size(); i++) {
+            if (dist[N][k - 1] < D) continue;
+            vector<pair<int, int> > near = map_[N];
 
-            int a = v[i].first;
-            int b = v[i].second;
+            for (int i = 0; i < near.size(); i++) {
+                    int a = near[i].first;
+                    int b = near[i].second;
 
-            if (d + b < dist[a][k - 1]) {
-                dist[a][k - 1] = d + b;
+                    if (D + b < dist[a][k - 1]) {
+                        dist[a][k - 1] = D + b;
+                        sort(dist[a].begin(), dist[a].end());
 
-                sort(dist[a].begin(), dist[a].end());
 
-                pq.push({ (d + b),a });
+                        pq.push({(D + b),a });
+                    }
             }
-        }
     }
 
 
@@ -66,7 +65,7 @@ int main(){
         for (int j=0; j<3; j++) cin >> edges[i][j]; //example for input: edges = {1, 2, 3} means the distance from 1 to 2 is 3.
 
     if (K == 1) cout << "The " << K << " shortest path is: ";
-    else cout << "The " << K << " shortest path are: ";
+    else cout << "The " << K << " shortest paths are: ";
 
     findkthshortest(N, M, K, edges);
     return 0;
